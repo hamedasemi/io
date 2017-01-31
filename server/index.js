90ca669c3b6bb197663c05b0fa7fe87243548310
Hamed Asemi <h-@live.se>
2017-01-31 22:01:16 +0100

    es6

--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
-var io = require('socket.io')(8080);
-var mySpace = io.of('/hamed-space/');
+import SocketIO from 'socket.io'
+
+const socket = new SocketIO(8080)
+
+let mySpace = socket.of('/hamed-space/')
 
 mySpace.on('connection', function (socket) {
     socket.on('log', function (x, y) {
